12fc40db7aaf86b2d31e43d57105277c26e7486f
Vinicius Bossle Fagundes <vinicius.bossle@ibridge.com.br>
2022-06-21 18:45:13 -0300

    validação conectividade graphcms via apollo

--- event-platform/src/App.tsx
+++ event-platform/src/App.tsx
@@ -1,4 +1,31 @@
+import { gql } from "@apollo/client"
+import { useEffect } from "react"
+import { client } from "./lib/apollo"
+
+const GET_LESSONS_QUERY = gql`
+query {
+  lessons {
+    id
+    slug
+    title
+    teacher {
+      name
+      bio
+      avatarURL
+    }
+  }
+}
+`
+
 function App() {
+  useEffect(() => {
+    client.query({
+      query: GET_LESSONS_QUERY
+    }).then(response =>{
+      console.log(response.data)
+    })
+  }, [])
+
   return (
     <h1 className="text-5xl font-bold text-violet-500">Hello World</h1>
   )

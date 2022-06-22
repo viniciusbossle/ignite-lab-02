5f0193d97513838608e087e1be8b95a7ba665a39
Vinicius Bossle Fagundes <vinicius.bossle@ibridge.com.br>
2022-06-22 08:56:35 -0300

    implementação do titulo no componente sidebar

--- event-platform/src/components/Sidebar.tsx
+++ event-platform/src/components/Sidebar.tsx
@@ -1,5 +1,11 @@
+import { Lesson } from "./Lesson";
+
 export function Sidebar() {
   return  (
-    <h1>Sidebar</h1>
+    <aside className="w-[384px] bg-gray-700 p-6 border-l border-gray-600">
+      <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
+        Cronograma de aulas
+      </span>
+    </aside>
   )
 }
--- event-platform/src/pages/Event.tsx
+++ event-platform/src/pages/Event.tsx
@@ -5,11 +5,12 @@ import { Video } from "../components/Video";
 
 export function Event() {
   return  (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Header />
-      <Sidebar />
-      <Video />
-      <Lesson />
+      <main className="flex flex-1">
+        <Video />
+        <Sidebar />
+      </main>
     </div>
   )
 }

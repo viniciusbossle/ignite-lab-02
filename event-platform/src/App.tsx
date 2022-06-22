90369af65efa6290e1c17801848ed0e82fa90710
Vinicius Bossle Fagundes <vinicius.bossle@ibridge.com.br>
2022-06-22 07:57:37 -0300

    criação dos componentes básicos da tela

--- event-platform/src/App.tsx
+++ event-platform/src/App.tsx
@@ -1,7 +1,15 @@
+import { Header } from "./components/Header"
+import { Sidebar } from "./components/Sidebar"
+import { Video } from "./components/Video"
+import { Lesson } from "./components/Lesson"
+
 function App() {
   return (
     <div>
-      
+      <Header />
+      <Sidebar />
+      <Video />
+      <Lesson />
     </div>
   )
 }

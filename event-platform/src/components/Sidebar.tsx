363d8aee4b4f5654cc6951d57f2f67e68474f5ae
Vinicius Bossle Fagundes <vinicius.bossle@ibridge.com.br>
2022-06-22 08:57:20 -0300

    implementação da Lesson no componente sidebar

--- event-platform/src/components/Sidebar.tsx
+++ event-platform/src/components/Sidebar.tsx
@@ -6,6 +6,14 @@ export function Sidebar() {
       <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
         Cronograma de aulas
       </span>
+      <div className="flex flex-col gap-8">
+        <Lesson />
+        <Lesson />
+        <Lesson />
+        <Lesson />
+        <Lesson />
+        <Lesson />
+      </div>
     </aside>
   )
 }
